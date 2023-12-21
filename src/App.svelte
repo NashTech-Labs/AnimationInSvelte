e2d352f8bd2459320e3f19be849481b804fa8669
Aanchal Agarwal <aanchal.agarwal@nashtechglobal.com>
2023-12-21 15:09:39 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { fade } from 'svelte/transition';
+
+  let visible = false;
+
+  function toggleVisibility() {
+    visible = !visible;
+  }
+</script>
+
+<style>
+  .box {
+    width: 100px;
+    height: 100px;
+    background-color: #3498db;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    font-size: 1.2rem;
+  }
+</style>
+
+<button on:click={toggleVisibility}>Toggle Visibility</button>
+
+{#if visible}
+  <div transition:fade class="box">
+    Hello, Svelte!
+  </div>
+{/if}
